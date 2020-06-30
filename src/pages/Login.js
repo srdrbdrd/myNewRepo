2c1638651c50704685300bca7d2df95473250996
EylemGokdemir05 <41906907+EylemGokdemir05@users.noreply.github.com>
2020-06-30 12:46:35 +0300

    Login and SignUp is ok.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,26 @@
 import React, {useState} from 'react'
-import {SafeAreaView,View,Text,TextInput,TouchableOpacity} from 'react-native'
-
+import {SafeAreaView,View,Text,TextInput,TouchableOpacity,Alert} from 'react-native'
+import auth from '@react-native-firebase/auth';
 import styles from '../styles'
 
 
 const Login=props=>{
+    const [usermail,setUsermail]=useState("")
+    const [password,setPassword]=useState("")
    
-   
-   
-   
-   
+    const setMail=text=>setUsermail(text) //maili alabilmek için oluşturulan fonksiyon
+    const setPass=text=>setPassword(text) //şifreyi alabilmek için oluşturulan fonksiyon
+
+
+    //kullanıcı girişi için çalışacak fonksiyon
+    const loginUser=async()=>{
+        try {
+            await auth().signInWithEmailAndPassword(usermail,password)
+            props.navigation.navigate("Main")
+        } catch (error) {
+            Alert.alert("App","Bir hata oluştu.")
+        }
+    }
    
    
     return(
@@ -20,18 +31,19 @@ const Login=props=>{
                     placeholder = "E-posta "
                     keyboardType = "email-address"
                     autoCapitalize = "none"
+                    onChangeText={setMail} //yazıyı değişirse yazıyı al
                     
                 />
 
                 <TextInput
                     placeholder = "Şifre"
                     secureTextEntry
-                    
+                    onChangeText={setPass} //yazıyı değişirse yazıyı al
 
                 />
 
                 <View style = {{marginTop: 20}}>
-                    <TouchableOpacity style = {[styles.login.buttonContainer,{backgroundColor:'#bbdefb'}]}>
+                    <TouchableOpacity style = {[styles.login.buttonContainer,{backgroundColor:'#bbdefb'}]} onPress={loginUser}>
                         <Text>Giriş</Text>
                     </TouchableOpacity> 
 
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,16 +1,32 @@
 import React, {useState} from 'react'
-import {SafeAreaView,View,Text,TextInput,TouchableOpacity} from 'react-native'
-
+import {SafeAreaView,View,Text,TextInput,TouchableOpacity,Alert} from 'react-native'
+import auth from '@react-native-firebase/auth';
 import styles from '../styles'
 
 
 const SignUp=props=>{
    
-   
-   
-   
-   
-   
+    const [usermail,setUsermail]=useState("")
+    const [password,setPassword]=useState("")
+    const [userpassRep,setUserpassrep]=useState("")
+
+    const setMail=text=>setUsermail(text)
+    const setPass=text=>setPassword(text)
+    const setPassRep=text=>setUserpassrep(text)
+
+    const signUser=async()=>{
+        if(password===userpassRep){
+            try {
+                await auth().createUserWithEmailAndPassword(usermail,password)
+                props.navigation.goBack()
+            } catch (error) {
+                Alert.alert("App","Bir hata oluştu, lütfen bilgileri kontrol edin.")
+            }
+        }
+        else{
+            Alert.alert("App","Şifreler uyuşmuyor!")
+        }
+    }
    
     return(
         <SafeAreaView style = {{flex:1}}>
@@ -20,13 +36,14 @@ const SignUp=props=>{
                     placeholder = "E-posta adresini giriniz"
                     keyboardType = "email-address"
                     autoCapitalize = "none"
-                    
+                    onChangeText={setMail}
                 />
 
                 <TextInput
                     style = {styles.login.input}
                     placeholder = "Şifrenizi giriniz"
                     secureTextEntry
+                    onChangeText={setPass}
                 />
 
 
@@ -34,12 +51,13 @@ const SignUp=props=>{
                     style = {styles.login.input}
                     placeholder = "Şifrenizi tekrar giriniz"
                     secureTextEntry
+                    onChangeText={setPassRep}
                 />
                     
 
 
                 <View style = {{marginTop: 20}}>
-                    <TouchableOpacity style = {[styles.login.buttonContainer,{backgroundColor:'#bbdefb'}]}>
+                    <TouchableOpacity style = {[styles.login.buttonContainer,{backgroundColor:'#bbdefb'}]} onPress={signUser}>
                         <Text>Kayıt Ol</Text>
                     </TouchableOpacity> 
 
