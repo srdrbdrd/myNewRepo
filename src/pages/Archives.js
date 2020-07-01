cf4ca826f6a6a515c8cae60279dff978774bf85c
srdrbdrd <serdarbedir97@hotmail.com>
2020-07-02 00:28:37 +0300

    Logout (Delete AsyncStorage User) added to Archive screen

--- src/pages/Archives.js
+++ src/pages/Archives.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import {SafeAreaView,View,Text} from 'react-native'
+import {SafeAreaView,View,Text, Button} from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage';
+import auth from '@react-native-firebase/auth';
+
+const Archives= (props) =>{
+    //Logout kısmını test amaçlı yaptım.Gerektiğinde logine geri dönmek için.
+    const user = auth().currentUser
+
+    const signOut = () => {
+        auth().signOut()
+        AsyncStorage.removeItem('@USER_ID')
+        props.navigation.navigate("Login")
+    }
 
 
-const Archives=props=>{
+
     return(
         <SafeAreaView>
         <View>
         <Text>Archives</Text>
+        <Button title = "Logout" onPress={signOut}/>
         </View>
         </SafeAreaView>
     )
